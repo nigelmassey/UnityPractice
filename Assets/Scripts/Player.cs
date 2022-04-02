23346f39c0844ddd858c6929f9c390d05130df0b
Nigel Massey <nigelmassey90@gmail.com>
2022-04-02 14:31:36 +0100

    Modified on ground check to use Physics.OverlapSphere

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,33 +6,32 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
     [SerializeField] private float speed = 3f;
-    [SerializeField] private float groundDistance = 0.2f;
-    [SerializeField] private LayerMask ground;
+    [SerializeField] private float playerMaskRadius = 0.07f;
+    [SerializeField] private LayerMask playerMask;
     [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private Transform GroundCheckObject;
 
     private Vector3 direction;
-    private bool isGrounded = true;
-    private Transform groundChecker;
+    private bool isInAir = false;    
     private Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        groundChecker = transform.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var horizontalMovement = Input.GetAxisRaw("Horizontal");
-        var verticalMovement = Input.GetAxisRaw("Vertical");
+        var horizontalMovement = Input.GetAxis("Horizontal");
+        var verticalMovement = Input.GetAxis("Vertical");
         var jumpKeyPressed = Input.GetKeyDown(KeyCode.Space);
 
-        isGrounded = Physics.CheckSphere(groundChecker.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
-        if (isGrounded && velocity.y < 0)
+        isInAir = Physics.OverlapSphere(GroundCheckObject.position, playerMaskRadius, playerMask).Length == 0;
+        if (!isInAir && velocity.y < 0)
             velocity.y = 0f;
 
-        if (isGrounded && jumpKeyPressed)
+        if (!isInAir && jumpKeyPressed)
         {
             velocity.y = 2f;
         }
